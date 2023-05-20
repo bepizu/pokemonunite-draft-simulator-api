71a953cf4c7c811b77616fd77744484db57c4459
Rafael Vilarinho Raiol <rafael.raiol@gmail.com>
2023-05-20 00:57:37 -0300

    update deploy action and create MONGODB_DATABASE env var

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,9 +1,10 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require('dotenv');
+dotenv.config();
 
 const environment = {
+  MONGODB_DATABASE: process.env.MONGODB_DATABASE || '',
   MONGODB_URI: process.env.MONGODB_URI || '',
-  PORT: process.env.PORT || 8001
-}
+  PORT: process.env.PORT || 8001,
+};
 
-module.exports = environment
+module.exports = environment;
