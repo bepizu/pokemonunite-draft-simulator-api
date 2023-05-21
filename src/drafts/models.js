7b6c6405dae37a4c8c7f9553354355ed08e9418a
Rafael Vilarinho Raiol <rafael.raiol@gmail.com>
2023-05-20 22:24:49 -0300

    fix reading/creating draft session

--- src/drafts/models.js
+++ src/drafts/models.js
@@ -1,28 +1,41 @@
-const { findOne, insert, update, IndexType } = require("../services/databases/mongodb")
+const { POKEMONS } = require('../config/constants');
+const {
+  findOne,
+  insert,
+  update,
+  IndexType,
+} = require('../services/databases/mongodb');
 
-
-async function getDraft({sessionId}) {
-  const query = {_id: new IndexType(sessionId)}
-  return await findOne("drafts", query)
+async function getDraft({ sessionId }) {
+  const query = { _id: new IndexType(sessionId) };
+  return await findOne('drafts', query);
 }
 
-async function createDraft({payload}) {
-  return await insert("drafts", payload)
+async function createDraft({ payload }) {
+  const pokemons = POKEMONS.filter((pkmn) => pkmn.active).map((pkmn) =>
+    Object.assign({}, pkmn)
+  );
+  return await insert('drafts', {
+    ...payload,
+    pokemons,
+    connections: { team1: '', team2: '' },
+    createdAt: new Date(),
+  });
 }
 
-async function updateDraft({sessionId, payload}) {
-  const query = {_id: new IndexType(sessionId)}
-  return await update("drafts", query, payload)
+async function updateDraft({ sessionId, payload }) {
+  const query = { _id: new IndexType(sessionId) };
+  return await update('drafts', query, { ...payload, updatedAt: new Date() });
 }
 
 async function initDraft({ sessionId, type }) {
-  const query = {_id: new IndexType(sessionId)}
-  return await findOne("drafts", query)
+  const query = { _id: new IndexType(sessionId) };
+  return await findOne('drafts', query);
 }
 
 module.exports = {
   getDraft,
   createDraft,
   updateDraft,
-  initDraft
-}
+  initDraft,
+};
